fe8d6fb515dc95bb28882622d7854b970d01036d
Manoj.Maracheea <manojM@wigroupinternational.com>
2018-12-03 14:39:47 +0400

    Added divide and multiply

--- calculator.py
+++ calculator.py
@@ -2,9 +2,18 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def substract(first_term, second_term):
     return first_term - second_term
-    
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
+
+
+def divide(first_term, second_term):
+    return first_term / second_term
