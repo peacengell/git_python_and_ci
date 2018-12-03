78a48ff21ee9dc7a66caa0bab20a4cd7b9ba2f6d
Manoj.Maracheea <manojM@wigroupinternational.com>
2018-12-03 14:40:35 +0400

    Added test

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.substract(4, 2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 50 == calculator.divide(100, 2)
